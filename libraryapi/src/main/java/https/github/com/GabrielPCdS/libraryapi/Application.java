33c4e504925ebd96a61653329e2084b3f2dea0c9
Gabriel Pinheiro Cardoso da Silva <gabriel.pinheirocds@gmail.com>
2026-06-30 18:29:53 -0300

    created project

---
+++ libraryapi/src/main/java/https/github/com/GabrielPCdS/libraryapi/Application.java
@@ -0,0 +1,13 @@
+package https.github.com.GabrielPCdS.libraryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ libraryapi/src/test/java/https/github/com/GabrielPCdS/libraryapi/ApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.GabrielPCdS.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
